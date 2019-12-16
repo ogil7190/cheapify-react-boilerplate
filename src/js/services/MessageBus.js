f305fc029e91483b72d04acc414ecfadc25f2fb8
Aman Kalra <Aman.Kalra@Amans-MBP.local>
2019-12-16 11:42:49 +0530

    Last Commit ;(

--- src/js/services/MessageBus.js
+++ src/js/services/MessageBus.js
@@ -1,44 +1,42 @@
-import { isEmpty } from 'lodash';
-const events = {};
+const _MessageBus = () => {
+    const events = {};
 
-const on = ( event, callback ) => {
-    const _events = events[ event ];
-    if ( _events ) {
-        _events.push( callback );
-    } else {
-        const _events = [];
-        _events.push( callback );
-        events.event = _events;
-    }
-};
-
-const off = ( event, callback ) => {
-    const _events = events[ event ];    // if event exists in the events array
-    if( _events ) {
-        const _index = _events.findIndexOf( callback );     // if callback exists in the given event
-        if( -1 !== _index ) {
-            _events.splice( _index, 1 );    // remove the function from the event list in case of unsubscribe
+    const on = ( event, callback ) => {
+        let _events = events[ event ];
+        if ( _events ) {
+            _events.push( callback );
+        } else {
+            _events = [];
+            _events.push( callback );
+            events.event = _events;
         }
-    }
-};
+    };
 
-/*  I guess we will have to use a 3rd parameter also to remove trigger a respective function only inside the array of events[event] remove this if read */
-const trigger = ( event, payload ) => {
-    const _events = events[ event ];
-        if( !isEmpty( _events ) ) {
-            _events.callback( payload );    //execute the callback of given event with its payload.
+    const off = ( event, callback ) => {
+        const _events = events[ event ];    // if event exists in the events array
+        if( _events ) {
+            const _index = _events.findIndexOf( callback );     // if callback exists in the given event
+            if( -1 !== _index ) {
+                _events.splice( _index, 1 );    // remove the function from the event list in case of unsubscribe
+            }
         }
+    };
+
+    /*  Execute all the functions that are contained in events [event] */
+    const trigger = ( event, payload ) => {
+        const _events = events[ event ];
+        const length = _events.length;
+        if( _events && 0 <  length ) {
+            _events.map( ( _event ) => {
+                _event( payload );
+            } );
+        }
+    };
+    return {
+        on,
+        off,
+        trigger
+    };
 };
 
-export const MessageBus ={};   // expose on, off and trigger functions of MessageBus
-MessageBus.on = ( event, callback ) => {
-    on( { event, callback } );
-};
-MessageBus.off = ( event, callback ) => {
-    off( { event, callback} );
-};
-MessageBus.trigger = ( event, payload ) => {
-    trigger( { event, payload} );
-};
-
-
+export const MessageBus = _MessageBus();
